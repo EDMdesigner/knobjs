78ce5cadfb00a4352068ef0fb32b1096dab2bb86
arnolduis <arnolduis@gmail.com>
2016-02-17 16:36:28 +0100

    RED-4405

---
+++ examples/knob.js
@@ -0,0 +1,67 @@
+/*jslint node: true */
+"use strict";
+
+var superdata = require("superdata");
+
+var createProxy = superdata.proxy.memory;
+var createModel = superdata.model.model;
+var createStore = superdata.store.store;
+
+var proxy = createProxy({
+	idProperty: "id",
+	route: "/user"
+});
+var model = createModel({
+	fields: {
+		id: {
+			type: "number"
+		},
+		email: {
+			type: "string"
+		},
+		name: {
+			type: "string"
+		},
+		title: {
+			type: "string"
+		}
+	},
+	proxy: proxy
+});
+var store = createStore({
+	model: model
+});
+
+//*
+//seed
+//atom
+var seed = true;
+function handleResponse(err, result) {
+	//console.log(err, result);
+}
+if (seed) {
+	var names = ["Bob", "Rob", "Olga", "Helga"];
+	var titles = ["CEO", "CTO", "Slave"];
+	for (var idx = 0; idx < 100; idx += 1) {
+		var actName = names[idx % 4];
+		store.add({
+			id: idx,
+			email: actName.toLowerCase() + "_" + idx + "@supercorp.com",
+			name: actName,
+			title: titles[idx % 3]
+		}, handleResponse);
+	}
+}
+
+window.store = store;
+
+var buttons = [];
+for (var idx = 0; idx < 10; idx += 1) {
+	buttons.push("button" + idx);
+}
+ko.applyBindings({
+	store: store,
+	numOfPages: ko.observable(),
+	numOfItems: ko.observable(1000),
+	buttons: buttons
+});
