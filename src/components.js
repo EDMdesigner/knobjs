a3e92da06b6a8ceb5e73d630a4547fc34ca7d0b7
arnolduis <arnolduis@gmail.com>
2016-02-17 16:36:04 +0100

    RED-4405

--- src/components.js
+++ src/components.js
@@ -1,58 +1,7 @@
 /*jslint node: true */
 "use strict";
-/*
-var createProxy = require("../proxy/memory");
-var createModel = require("../model/model");
-var createStore = require("../store/store");
 
-var proxy = createProxy({
-	idProperty: "id",
-	route: "/user"
-});
-var model = createModel({
-	fields: {
-		id: {
-			type: "number"
-		},
-		email: {
-			type: "string"
-		},
-		name: {
-			type: "string"
-		},
-		title: {
-			type: "string"
-		}
-	},
-	proxy: proxy
-});
-var store = createStore({
-	model: model
-});
-
-//*
-//seed
-//atom
-var seed = true;
-function handleResponse(err, result) {
-	//console.log(err, result);
-}
-if (seed) {
-	var names = ["Bob", "Rob", "Olga", "Helga"];
-	var titles = ["CEO", "CTO", "Slave"];
-	for (var idx = 0; idx < 100; idx += 1) {
-		var actName = names[idx % 4];
-		store.add({
-			id: idx,
-			email: actName.toLowerCase() + "_" + idx + "@supercorp.com",
-			name: actName,
-			title: titles[idx % 3]
-		}, handleResponse);
-	}
-}
-
-window.store = store;
-*/
+//*/
 
 //THIS FILE SHOULD BE GENERATED
 
