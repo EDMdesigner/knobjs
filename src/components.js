b53dcc7582e843a8d36b98b9dfe0bb44a948a6bd
Gyula Nemeth <gyula.nemeth85@gmail.com>
2016-02-18 17:00:14 +0100

    RED-4413

--- src/components.js
+++ src/components.js
@@ -9,7 +9,8 @@ var registerComponent = require("./knobRegisterComponent");
 
 registerComponent("knob-button", require("./button/vm"), require("./button/template.html"), require("./button/style.js"));
 registerComponent("knob-input", require("./input/vm"), require("./input/template.html"), require("./input/style.js"));
-registerComponent("knob-dropdown", require("./dropdown/vm"), require("./dropdown/template.html")/*, require("./button/style.json")*/);
+registerComponent("knob-inline-text-editor", require("./inlineTextEditor/vm"), require("./inlineTextEditor/template.html"));
+registerComponent("knob-dropdown", require("./dropdown/vm"), require("./dropdown/template.html"));
 registerComponent("knob-pagination", require("./pagination/vm"), require("./pagination/template.html"));
 registerComponent("knob-items-per-page", require("./itemsPerPage/vm"), require("./itemsPerPage/template.html"));
 registerComponent("knob-paged-list", require("./pagedList/vm"), require("./pagedList/template.html"));
---
+++ src/inlineTextEditor/vm.js
@@ -0,0 +1,31 @@
+/*jslint node: true */
+"use strict";
+
+var ko = require("knockout");
+
+function createInlineTextEditor(config) {
+	var vm = {};
+
+	vm.value = config.value || ko.observable("");
+	vm.editedValue = ko.observable(vm.value());
+
+	vm.editMode = ko.observable(false);
+
+	vm.edit = function() {
+		vm.editedValue(vm.value());
+		vm.editMode(true);
+	};
+
+	vm.save = function() {
+		vm.value(vm.editedValue());
+		vm.editMode(false);
+	};
+
+	vm.cancel = function() {
+		vm.editMode(false);
+	};
+
+	return vm;
+}
+
+module.exports = createInlineTextEditor;
