09c55861853c14c14686bf5aadef57ed4afee782
Gyula Nemeth <gyula.nemeth85@gmail.com>
2016-02-22 08:38:42 +0100

    RED-4425

---
+++ src/tabs/vm.js
@@ -0,0 +1,17 @@
+/*jslint node: true */
+"use strict";
+
+var ko = require("knockout");
+
+function createRadio() {
+	var vm = {};
+
+
+	//child templates
+	// - params - label, etc
+	// - based on that we can build a knob-radio-button
+
+	return vm;
+}
+
+module.exports = createRadio;
