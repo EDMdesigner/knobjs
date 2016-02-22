aadf7e8b6c0d291533691d8a29a8b8f5cb7c9743
Gyula Nemeth <gyula.nemeth85@gmail.com>
2016-02-22 08:35:17 +0100

    RED-4427

--- src/components.js
+++ src/components.js
@@ -6,6 +6,7 @@
 //THIS FILE SHOULD BE GENERATED
 
 var registerComponent = require("./knobRegisterComponent");
+var baseVm = require("./base/vm");
 
 module.exports = function initKnob(theme) {
 	registerComponent("knob-button", require("./button/vm"), require("./button/template.html"), require("./button/style.js")(theme));
@@ -16,5 +17,12 @@ module.exports = function initKnob(theme) {
 	registerComponent("knob-pagination", require("./pagination/vm"), require("./pagination/template.html"));
 	registerComponent("knob-items-per-page", require("./itemsPerPage/vm"), require("./itemsPerPage/template.html"));
 	registerComponent("knob-paged-list", require("./pagedList/vm"), require("./pagedList/template.html"));
+
+	return {
+		registerComponent: registerComponent,
+		base: {
+			vm: baseVm
+		}
+	};
 };
 //
