36f93fba407f0bc07ad7295bd51d12544f0cf64f
ityu <nagyityuistvan@gmail.com>
2016-03-10 19:39:54 +0100

    RED-4524

---
+++ spec/base/behaviours/clickSpec.js
@@ -0,0 +1,72 @@
+var ko = require("knockout");
+var hoverBehaviour = require("../../../src/base/behaviours/click");
+
+describe("click behaviour", function() {
+
+	describe("with invalid config", function() {
+		it(" - falsy", function() {
+			expect(hoverBehaviour).toThrowError("vm is mandatory!");
+		});
+
+		it(" - config.state is not observable", function() {
+			expect(function() {
+				hoverBehaviour({});
+			}).toThrowError("vm.state has to be a knockout observable!");
+
+			expect(function() {
+				hoverBehaviour({
+					state: function() {}
+				});
+			}).toThrowError("vm.state has to be a knockout observable!");
+		});
+	});
+
+	describe("with valid config", function() {
+		var vm;
+		var actState = "hover";
+
+		beforeEach(function() {
+			vm = {
+				state: ko.observable(actState)
+			};
+			hoverBehaviour(vm);
+		});
+
+		it("interface check", function() {
+			expect(ko.isObservable(vm.state)).toBe(true);
+			expect(typeof vm.eventHandlers).toBe("object");
+			expect(typeof vm.eventHandlers.mousedown).toBe("function");
+			expect(typeof vm.eventHandlers.mouseup).toBe("function");
+		});
+
+		it("should set state to 'click' on mousedown call", function() {
+			vm.eventHandlers.mousedown();
+			expect(vm.state()).toBe("active");
+		});
+
+		it("should set state to the previous state on mouseup call", function() {
+			vm.eventHandlers.mousedown();
+			expect(vm.state()).toBe("active");
+			vm.eventHandlers.mouseup();
+			expect(vm.state()).toBe("hover");
+		});
+
+		it("should not set the state on mousedown call when the state is 'disabled' or 'active'", function() {
+			vm.state("disabled");
+
+			vm.eventHandlers.mousedown();
+			expect(vm.state()).toBe("disabled");
+
+			vm.eventHandlers.mouseup();
+			expect(vm.state()).toBe("disabled");
+
+			vm.state("active");
+
+			vm.eventHandlers.mousedown();
+			expect(vm.state()).toBe("active");
+
+			vm.eventHandlers.mouseup();
+			expect(vm.state()).toBe("hover");
+		});
+	});
+});
--- src/base/behaviours/click.js
+++ src/base/behaviours/click.js
@@ -1,7 +1,17 @@
 /*jslint node: true */
 "use strict";
 
+var ko = require("knockout");
+
 module.exports = function clickBehaviour(vm) {
+	if (!vm) {
+		throw new Error("vm is mandatory!");
+	}
+
+	if (!ko.isObservable(vm.state)) {
+		throw new Error("vm.state has to be a knockout observable!");
+	}
+
 	function mouseDown() {
 		var actState = vm.state();
 
