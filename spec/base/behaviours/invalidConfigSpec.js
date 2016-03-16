88a191b38f827765eb9452769df2726584f34040
iTyu <nagyityuistvan@gmail.com>
2016-03-16 10:08:18 +0100

    RED-4524 RED-4525

---
+++ spec/base/behaviours/invalidConfigSpec.js
@@ -0,0 +1,22 @@
+module.exports = function invalidConfig(config) {
+
+	var behaviour = config.behaviour;
+
+	describe("with invalid config", function() {
+		it(" - falsy", function() {
+			expect(behaviour).toThrowError("vm is mandatory!");
+		});
+
+		it(" - config.state is not observable", function() {
+			expect(function() {
+				behaviour({});
+			}).toThrowError("vm.state has to be a knockout observable!");
+
+			expect(function() {
+				behaviour({
+					state: function() {}
+				});
+			}).toThrowError("vm.state has to be a knockout observable!");
+		});
+	});
+};
--- src/base/behaviours/focus.js
+++ src/base/behaviours/focus.js
@@ -1,7 +1,18 @@
 /*jslint node: true */
 "use strict";
 
+var ko = require("knockout");
+
 module.exports = function focusBehaviour(vm) {
+
+	if (!vm) {
+		throw new Error("vm is mandatory!");
+	}
+
+	if (!ko.isObservable(vm.state)) {
+		throw new Error("vm.state has to be a knockout observable!");
+	}
+
 	function focus() {
 		var actState = vm.state();
 
