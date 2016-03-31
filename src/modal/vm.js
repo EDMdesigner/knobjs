afae01840be16e78345d80c1476cbd0c162ee259
iTyu <nagyityuistvan@gmail.com>
2016-03-31 14:55:08 +0200

    RED-4599

--- src/modal/vm.js
+++ src/modal/vm.js
@@ -1,24 +1,28 @@
 "use strict";
 
-var ko = require("knockout");
+var base = require("../base/vm");
 
 function createModal(config) {
+
+	config = config || {};
 
 	var visible = config.visible;
 	var title = config.title;
 	var icon = config.icon;
 
-
-
 	visible.toggle = function() {
 		visible(!visible());
 	};
 
-	return {
-		visible: visible,
-		title: title,
-		icon: icon
-	};
+	config.component = "modal";
+
+	var vm = base(config);
+
+	vm.visible = visible;
+	vm.title = title;
+	vm.icon = icon;
+
+	return vm;
 }
 
 module.exports = createModal;
