8498f87d3167293ae6ec2ddd58721c96976c1e1d
Gyula Nemeth <gyula.nemeth85@gmail.com>
2016-04-01 10:52:52 +0200

    RED-4607, RED-4608

--- src/modal/confirm/vm.js
+++ src/modal/confirm/vm.js
@@ -4,17 +4,33 @@ function createConfirmModal(config) {
 	var visible = config.visible;
 	var callback = config.callback;
 
+	var title = config.title;
+	var icon = config.icon;
+	var message = config.message;
+
+	var okLabel = config.okLabel;
+	var cancelLabel = config.cancelLabel;
+
 
 	function ok() {
 		callback(true);
+		visible.toggle();
 	}
 
 	function cancel() {
 		callback(false);
+		visible.toggle();
 	}
 
 	return {
 		visible: visible,
+
+		title: title,
+		icon: icon,
+		message: message,
+
+		okLabel: okLabel,
+		cancelLabel: cancelLabel,
 
 		ok: ok,
 		cancel: cancel
