83157656314b4c0f7cf5997bc4bfe07f1a0ffa70
arnolduis <arnolduis@gmail.com>
2016-04-07 13:56:18 +0200

    RED-4638

--- src/pagedList/vm.js
+++ src/pagedList/vm.js
@@ -6,10 +6,13 @@ var createList = require("../list/vm");
 
 module.exports = function createPagedList(config) {
 	config = config || {};
-	
+
 	if (!config.store) {
 		throw new Error("config.store is mandatory!");
 	}
+
+
+
 
 	var store = config.store;
 
@@ -23,6 +26,8 @@ module.exports = function createPagedList(config) {
 	var itemsPerPage = ko.observable(10);
 	var currentPage = ko.observable(0);
 
+	list.listClass = config.listClass || "knob-pagedlist__list";
+	list.itemClass = config.itemClass || "knob-pagedlist__item";
 	list.numOfPages = numOfPages;
 	list.itemsPerPage = itemsPerPage;
 	list.currentPage = currentPage;
