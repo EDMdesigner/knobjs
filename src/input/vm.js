d26e57683bc2d983de2b15dac3b1f4612f222fbe
iTyu <nagyityuistvan@gmail.com>
2016-04-08 10:33:19 +0200

    RED-4640 - placeholder

--- src/input/vm.js
+++ src/input/vm.js
@@ -21,12 +21,14 @@ function createInput(config) {
 
 	config.component = "input";
 	config.type = config.type || "text";
+	config.placeholder = config.placeholder || "";
 
 	var vm = base(config);
 
 	vm.behaviours.hover.enable();
 	vm.behaviours.focus.enable();
 
+	vm.placeholder = config.placeholder;
 	vm.type = config.type;
 	vm.value = config.value || ko.observable();
 	vm.hasFocus = config.hasFocus || ko.observable(false);
