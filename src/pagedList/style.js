7a27c4a2d2a023edfb4afa66bff2cb53a7fb789b
iTyu <nagyityuistvan@gmail.com>
2016-04-19 09:50:40 +0200

    RED-4699 RED-4700 RED-4645

--- src/pagedList/style.js
+++ src/pagedList/style.js
@@ -3,34 +3,24 @@ module.exports = function createStyleConfig(theme) {
 	var tinycolor = require("tinycolor2");
 
 	return {
-		"default": {
+		"even": {
 			"default": {
 				"backgroundColor": theme.white,
-				"color": theme.black,
-				"border-color": theme.mediumGray
+				"color": theme.black
 			},
 			"hover": {
-				"backgroundColor": tinycolor(theme.white).darken().toString(),
-				"color": theme.black,
-				"border-color": tinycolor(theme.mediumGray).darken.toString()
-			},
-			"active": {
-				"color": tinycolor(theme.mediumGray).darken.toString(),
-				"fill": tinycolor(theme.mediumGray).darken.toString()
-			},
-			"disabled": {
+				"backgroundColor": theme.mediumGray,
+				"color": theme.black
+			}
+		},
+		"odd": {
+			"default": {
 				"backgroundColor": theme.lightGray,
 				"color": theme.black
 			},
-			"success": {
-				"backgroundColor": theme.successColor,
-				"color": theme.white,
-				"fill": theme.white
-			},
-			"error": {
-				"backgroundColor": theme.alertColor,
-				"color": theme.white,
-				"fill": theme.white
+			"hover": {
+				"backgroundColor": theme.mediumGray,
+				"color": theme.black
 			}
 		}
 	};
