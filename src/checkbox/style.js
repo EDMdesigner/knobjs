3af89e96c0e4778e061b71a6558bc56ec62b774c
kohalmyk <kohalmyk@gmail.com>
2016-06-30 17:44:20 +0200

    Added knob-checkbox component with tests, documentation and example. https://github.com/EDMdesigner/knobjs/issues/25

---
+++ src/checkbox/style.js
@@ -0,0 +1,29 @@
+module.exports = function createStyleConfig(theme) {
+
+	var tinycolor = require("tinycolor2");
+
+	return {
+		"default": {
+			"default": {
+				"backgroundColor": theme.secondaryColor,
+				"borderColor": theme.secondaryColor,
+				"color": theme.black,
+				"fill": theme.black
+			},
+			"hover": {
+				"backgroundColor": tinycolor(theme.secondaryColor).lighten().toString(),
+				"borderColor": tinycolor(theme.secondaryColor).lighten().toString(),
+				"fill": tinycolor(theme.secondaryColor).darken().darken().toString()
+			},
+			"active": {
+				"backgroundColor": tinycolor(theme.secondaryColor).darken().toString(),
+				"borderColor": tinycolor(theme.secondaryColor).darken().toString()
+			},
+			"disabled": {
+				"backgroundColor": theme.mediumGray,
+				"color": theme.lightGray,
+				"fill": theme.lightGray
+			}
+		}
+	};
+};
