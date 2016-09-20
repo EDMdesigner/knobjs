b4f7bb75272594c4af29d25bf85fa62de9167e3e
Kőhalmy Kristóf <kohalmyk@gmail.com>
2016-09-20 07:42:14 +0200

    knob input with primary variation for chamaileon style also

--- src/input/chamaileon.js
+++ src/input/chamaileon.js
@@ -1,4 +1,8 @@
+var tinycolor = require("tinycolor2");
+
 module.exports = function createStyleConfig(theme) {
+	var hoverColor = tinycolor(theme.primaryColor).isDark() ? tinycolor(theme.primaryColor).lighten(20).toString() : tinycolor(theme.primaryColor).darken(20).toString();
+	var activeColor = tinycolor(theme.primaryColor).isDark() ? tinycolor(theme.primaryColor).lighten(30).toString() : tinycolor(theme.primaryColor).darken(30).toString();
 	return {
 		"default": {
 			"default": {
@@ -15,6 +19,31 @@ module.exports = function createStyleConfig(theme) {
 				"backgroundColor": theme.white,
 				"color": theme.primaryColor,
 				"fill": theme.primaryColor
+			},
+			"disabled": {
+				"backgroundColor": theme.lightGray,
+				"color": theme.lightGray,
+				"fill": theme.lightGray
+			}
+		},
+		"primary": {
+			"default": {
+				"backgroundColor": theme.white,
+				"color": theme.black,
+				"border-color": theme.primaryColor,
+				"fill": theme.primaryColor
+			},
+			"hover": {
+				"backgroundColor": theme.white,
+				"color": theme.black,
+				"border-color": hoverColor,
+				"fill": hoverColor
+			},
+			"active": {
+				"backgroundColor": theme.white,
+				"color": theme.black,
+				"border-color": activeColor,
+				"fill": activeColor
 			},
 			"disabled": {
 				"backgroundColor": theme.lightGray,
