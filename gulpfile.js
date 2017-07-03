ea77debca3e985c6e879cfed5cfd3c3d2f4afd6b
arnolduis <arnolduis@gmail.com>
2017-07-03 21:15:22 +0200

    supergulp integration

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,7 @@ var superGulp = createSuperGulp({
 });
 
 var packageJson = require("./package.json");
-/*
-var resourceConfigs = {
-	dev: __dirname + "/src/config-superpreview-dev.json",
-	staging: __dirname + "/src/config-superpreview-staging.json",
-	prod: __dirname + "/src/config-superpreview-prod.json"
-};
-*/
+
 var jsFiles = [
 	"./*.js",
 	"./src/**/*.js",
@@ -38,21 +32,7 @@ var specFiles = [
 var sourceFiles = [
 	"src/**/*.js"
 ];
-/*
-var localDeps = [
-	"./node_modules/knob-js/dist/knob.js",
-	"./node_modules/knob-js/dist/knob.min.css",
-	"./node_modules/normalize.css/normalize.css",
-	"./node_modules/knockout/build/output/knockout-latest.debug.js",
-];
 
-var deps = [
-	"./src/public/img/ipad-white-portrait-final2.png",
-	"./src/public/img/iphone5s-white-portrait-final2.png",
-	"./src/public/img/macbookpro-final2.png",
-	"./src/public/img/macbookpro-final2b.png"
-];
-*/
 var deployFiles = [
 	"!./dist/examples",
 	"!./dist/lib",
@@ -60,9 +40,9 @@ var deployFiles = [
 ];
 
 
-//
-// //SVG sprite generate
-//
+
+//SVG sprite generate
+
 // gulp.task("svg", function () {
 // 	var svgs = gulp.src("examples/SVG/*.svg")
 // 		.pipe(svgstore({ inlineSvg: true }));
@@ -75,87 +55,7 @@ var deployFiles = [
 // 		.pipe(inject(svgs, { transform: fileContents }))
 // 		.pipe(gulp.dest("examples/"));
 // });
-//
-//
-// // Watchers
-// // ==================================================
-// gulp.task("sass:watch", function() {
-// 	gulp.watch("./src/**/*.scss", ["sass:dev"]);
-// });
-//
-// gulp.task("js:watch", function() {
-// 	gulp.watch(["./src/**/*.js", "./examples/**/*.js", "./src/**/*.html", "./examples/**/*.html", "./src/**/*.json"], ["js:dev"])
-// 		.on("change", function(event) {
-// 			console.log(event);
-// 		});
-// });
-/*
-gulp.task("s3-deploy", function() {
-	//console.log(gutil.env);
-	if (!gutil.env.s3key || !gutil.env.s3secret || !gutil.env.s3region || !gutil.env.s3bucket) {
-		throw new Error("s3key, s3secret, s3region and s3bucket is mandatory! Use command like this: gulp s3-deploy --s3key 'S3 KEY' --s3secret 'S3 SECRET' --s3region 'S3 region' --s3bucket 'S3 bucket'!");
-	}
 
-	var s3 = new AWS.S3({
-		accessKeyId: gutil.env.s3key,
-		secretAccessKey: gutil.env.s3secret,
-		region: gutil.env.s3region,
-		params: {
-			Bucket: gutil.env.s3bucket
-		}
-	});
-	var version = packageJson.version;
-	var destFolder = version + "/";
-	var files = [
-		"./dist/knob.js",
-		"./dist/knob.min.css"
-	];
-	var createUploadFunction = function(currentFile) {
-		return function(err, data) {
-			if (err) {
-				throw new Error(err);
-			}
-			var putObjectConfig = {
-				Key: destFolder + currentFile.replace("./dist/", "").replace("./examples/", "").replace("./", ""),
-				Bucket: gutil.env.s3bucket,
-				ACL: "public-read",
-				Body: data,
-				ContentType: mime.lookup(currentFile)
-			};
-			s3.putObject(putObjectConfig, function(err, awsResult) {
-				if (err) {
-					throw new Error(err);
-				}
-				console.log("File '" + currentFile + "' uploaded successfully to S3!");
-				console.log(awsResult);
-			});
-		};
-	};
-
-	var params = {
-		Bucket: gutil.env.s3bucket,
-		Key: destFolder + "editor.js"
-	};
-	s3.headObject(params, function (err) {
-		if (!err || err.code !== "NotFound") {
-			var errorMessage = "Version is already published! Change version before deployment! Checked file: '" + params.Key + "'.";
-			if (err && err.code) {
-				errorMessage += " Returned error when testing: '" + err.code + "' (instead of 'NotFound')";
-			}
-			throw new Error(errorMessage);
-		}
-
-		var currentFile;
-
-		for (var i = 0; i < files.length; i += 1) {
-			currentFile = files[i];
-			fs.readFile(currentFile, createUploadFunction(currentFile));
-		}
-	});
-});
-
-
-*/
 superGulp.taskTemplates.initPluginTasks({
 	packageJson: packageJson,
 	coverage: 70,
@@ -171,6 +71,8 @@ superGulp.taskTemplates.initPluginTasks({
 	tasks: {
 		copy:{
 			dev: [
+				{files: "./node_modules/normalize.css/normalize.css", dest: "./dist/lib"},
+				{files: "./node_modules/knockout/build/output/knockout-latest.debug.js", dest: "./dist/lib"},
 				{files: "./examples/example.html", dest: "./dist/examples"}
 			]
 		},
