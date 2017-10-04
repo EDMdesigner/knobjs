42034590653ff372df1e490b2e315c3952801b39
Anikó Borosová <borosanikoo@gmail.com>
2017-10-04 10:44:28 +0200

    update helpBox vm + spec

--- src/components/helpBox/vm.js
+++ src/components/helpBox/vm.js
@@ -2,21 +2,22 @@
 
 var ko = require("knockout");
 
-function createHelpBox() {
+function createHelpBox() {	
+
 	var vm = {};
-	
-		vm.boxEnabled = ko.observable(true);
-		vm.infoEnabled = ko.observable(false);
 
-		vm.hideBox = function() {
-			vm.boxEnabled(false);
-			vm.infoEnabled(true);
+	vm.boxEnabled = ko.observable(true);
+	vm.infoEnabled = ko.observable(false);
 
-		};
-		vm.showBox = function() {
-			vm.boxEnabled(true);
-			vm.infoEnabled(false);
-		};
+	vm.hideBox = function() {
+		vm.boxEnabled(false);
+		vm.infoEnabled(true);
+
+	};
+	vm.showBox = function() {
+		vm.boxEnabled(true);
+		vm.infoEnabled(false);
+	};
 
 	return vm;
 }
